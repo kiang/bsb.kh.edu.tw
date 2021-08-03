8eab2baebf4520dfebc2468c90f8b6e977207493
kiang <kiange@gmail.com>
2021-08-03 17:54:58 +0800

    add empty count

--- scripts/02_calc.php
+++ scripts/02_calc.php
@@ -2,6 +2,7 @@
 $count = [
     'total' => 0,
     'count' => 0,
+    'emptyCount' => 0,
 ];
 foreach(glob(dirname(__DIR__) . '/data/raw/*/*.json') AS $jsonFile) {
     $json = json_decode(file_get_contents($jsonFile), true);
@@ -14,6 +15,8 @@ foreach(glob(dirname(__DIR__) . '/data/raw/*/*.json') AS $jsonFile) {
         if(count($json['職員工']) === 1 && $classCount > 10) {
             $count['count']++;
         }
+    } else {
+        $count['emptyCount']++;
     }
 }
 print_r($count);
