14d6b77dec897845dcc83aec2dd065e9a0481d0f
kiang <kiange@gmail.com>
2021-08-18 18:11:01 +0800

    add cron script

---
+++ scripts/cron.php
@@ -0,0 +1,16 @@
+<?php
+$rootPath = dirname(__DIR__);
+
+$now = date('Y-m-d H:i:s');
+
+exec("cd {$rootPath} && /usr/bin/git pull");
+
+exec("php -q {$rootPath}/scripts/01_fetch_raw.php");
+exec("php -q {$rootPath}/scripts/01_fetch_detail.php");
+exec("php -q {$rootPath}/scripts/03_geocoding.php");
+
+exec("cd {$rootPath} && /usr/bin/git add -A");
+
+exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+
+exec("cd {$rootPath} && /usr/bin/git push origin master");
