e658ec7503e18cd95c0ae717538a725db1d62b4f
kiang <kiange@gmail.com>
2024-06-14 16:41:52 +0800

    add sleep in each 8 fetching

--- scripts/01_fetch_detail.php
+++ scripts/01_fetch_detail.php
@@ -1,14 +1,15 @@
 <?php
 $header1 = ['核准科目名稱', '核准班級數', '每班核准人數', '每週總節(時)數', '修業期限', '招生對象'];
-foreach(glob(dirname(__DIR__) . '/data/raw/*.csv') AS $csvFile) {
+$sleepCount = 0;
+foreach (glob(dirname(__DIR__) . '/data/raw/*.csv') as $csvFile) {
     $p = pathinfo($csvFile);
     $dataPath = dirname(__DIR__) . '/data/raw/' . $p['filename'];
-    if(!file_exists($dataPath)) {
+    if (!file_exists($dataPath)) {
         mkdir($dataPath, 0777);
     }
     $fh = fopen($csvFile, 'r');
     $header = fgetcsv($fh, 2048);
-    while($line = fgetcsv($fh, 2048)) {
+    while ($line = fgetcsv($fh, 2048)) {
         $data = [];
         $page1 = file_get_contents('https://bsb.kh.edu.tw/afterschool/register/detail.jsp?unit=' . $line[0]);
         $partPos = strpos($page1, '<table');
@@ -16,22 +17,22 @@ foreach(glob(dirname(__DIR__) . '/data/raw/*.csv') AS $csvFile) {
         $part1 = substr($page1, $partPos, $partPosEnd - $partPos);
         $part1 = str_replace('</tr>', '</td>', $part1);
         $rows = explode('</td>', $part1);
-        foreach($rows AS $row) {
+        foreach ($rows as $row) {
             $rowParts = explode('</th>', $row);
-            if(count($rowParts) === 2) {
-                foreach($rowParts AS $k => $v) {
+            if (count($rowParts) === 2) {
+                foreach ($rowParts as $k => $v) {
                     $rowParts[$k] = trim(strip_tags($v));
                 }
-                $data[$rowParts[0]] = $rowParts[1];    
+                $data[$rowParts[0]] = $rowParts[1];
             }
         }
         $data['負責人'] = [];
         $partPos = strpos($page1, '<table', $partPosEnd);
         $partPosEnd = strpos($page1, '</table>', $partPos);
         $partLines = explode('</tr>', substr($page1, $partPos, $partPosEnd - $partPos));
-        foreach($partLines AS $partLine) {
+        foreach ($partLines as $partLine) {
             $partCols = explode('<td class="listBody">', $partLine);
-            if(count($partCols) === 2) {
+            if (count($partCols) === 2) {
                 $data['負責人'][] = trim(strip_tags($partCols[1]));
             }
         }
@@ -40,9 +41,9 @@ foreach(glob(dirname(__DIR__) . '/data/raw/*.csv') AS $csvFile) {
         $partPos = strpos($page1, '<table', $partPosEnd);
         $partPosEnd = strpos($page1, '</table>', $partPos);
         $partLines = explode('</tr>', substr($page1, $partPos, $partPosEnd - $partPos));
-        foreach($partLines AS $partLine) {
+        foreach ($partLines as $partLine) {
             $partCols = explode('<td class="listBody">', $partLine);
-            if(count($partCols) === 2) {
+            if (count($partCols) === 2) {
                 $data['設立人'][] = trim(strip_tags($partCols[1]));
             }
         }
@@ -51,9 +52,9 @@ foreach(glob(dirname(__DIR__) . '/data/raw/*.csv') AS $csvFile) {
         $partPos = strpos($page1, '<table', $partPosEnd);
         $partPosEnd = strpos($page1, '</table>', $partPos);
         $partLines = explode('</tr>', substr($page1, $partPos, $partPosEnd - $partPos));
-        foreach($partLines AS $partLine) {
+        foreach ($partLines as $partLine) {
             $partCols = explode('<td class="listBody">', $partLine);
-            if(count($partCols) === 2) {
+            if (count($partCols) === 2) {
                 $data['班主任'][] = trim(strip_tags($partCols[1]));
             }
         }
@@ -62,10 +63,10 @@ foreach(glob(dirname(__DIR__) . '/data/raw/*.csv') AS $csvFile) {
         $partPos = strpos($page1, '<table', $partPosEnd);
         $partPosEnd = strpos($page1, '</table>', $partPos);
         $partLines = explode('</tr>', substr($page1, $partPos, $partPosEnd - $partPos));
-        foreach($partLines AS $partLine) {
+        foreach ($partLines as $partLine) {
             $partCols = explode('<td width="14%" class="listBody">', $partLine);
-            if(count($partCols) === 6) {
-                foreach($partCols AS $k => $v) {
+            if (count($partCols) === 6) {
+                foreach ($partCols as $k => $v) {
                     $partCols[$k] = trim(strip_tags($v));
                 }
                 $data['核准科目'][] = array_combine($header1, $partCols);
@@ -75,13 +76,17 @@ foreach(glob(dirname(__DIR__) . '/data/raw/*.csv') AS $csvFile) {
         $page2 = file_get_contents('https://bsb.kh.edu.tw/afterschool/teacher/new/staff_list.jsp?unit=' . $line[0]);
         $data['職員工'] = [];
         $staffLines = explode('</tr>', $page2);
-        foreach($staffLines AS $staffLine) {
+        foreach ($staffLines as $staffLine) {
             $cols = explode('</td>', $staffLine);
-            if(count($cols) === 3) {
+            if (count($cols) === 3) {
                 $data['職員工'][] = trim(strip_tags($cols[1]));
             }
         }
         file_put_contents($dataPath . '/' . $line[0] . '.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         echo "{$line[0]}\n";
+        if (++$sleepCount > 3) {
+            sleep(1);
+            $sleepCount = 0;
+        }
     }
 }
